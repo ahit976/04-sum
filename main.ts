322367ecdb8de42ed9f9808b254915b886be7e5e
ahit976 <55730608+ahit976@users.noreply.github.com>
2023-02-19 21:06:31 +0800

    MakeCode 项目的初始文件

---
+++ main.ts
@@ -0,0 +1,16 @@
+input.onButtonPressed(Button.AB, function () {
+    sum = 0
+    a = 1
+    for (let index = 0; index < 10; index++) {
+        sum = sum + a
+        a += 1
+    }
+    basic.showNumber(sum)
+})
+let a = 0
+let sum = 0
+sum = 0
+a = 1
+basic.forever(function () {
+	
+})
